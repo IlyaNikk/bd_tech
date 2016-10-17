582bf905c503b0d0df11e540448dbc5d8ec9508a
IlyaNikk <vic096@yandex.ru>
2016-10-18 01:29:20 +0300

    need to add three methods in user

---
+++ mysql.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let mysql = require('promise-mysql');
+let connection;
+
+exports.getConnection = function () {
+	return new Promise(function (resolve, reject) {
+		if (connection) {
+			resolve(connection);
+		} else {
+			mysql
+				.createConnection({
+					host: 'localhost',
+					user: 'root',
+					password: 'Computer1',
+					database: 'forum'
+				})
+				.then(conn => {
+					connection = conn;
+					resolve(conn);
+				})
+				.catch(reject);
+		}
+	});
+};
